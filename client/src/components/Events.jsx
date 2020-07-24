63a33a5763604ae21c58be070546641e0f723d01
JP Meyers <johnpmeyers20@yahoo.com>
2020-07-24 10:31:33 -0400

    added styling to events component

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -1,26 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Banner from './banner/Banner';
 
 export default function Events(props) {
   const { events } = props;
   // console.log("HEREEEE", events);
 
   return (
+    <>
+    <Banner />
     <div className='events-container'>
       <div className='row'>
-        <h1>STADIO</h1>
+        <span><h1>STADIO</h1></span>
         <img src='https://i.imgur.com/dYLT2Ri.png' alt='profile icon' />
       </div>
+      <div className='my-events-container'>
+        <p>My Events</p>
+      </div>
       {events.map((event) => (
-      <div key={event.id}>
-        <Link to={`events/${event.id}`}>
-          <p>{event.name}</p>
-          <p>{event.date}</p>
-          <p>{event.time}</p>
-          <p>Citi Field</p>
+      <div className='act-event' key={event.id}>
+        <Link to={`events/${event.id}`} style={{ textDecoration: 'none' }}>
+          <div className='event-name'>
+            <h2>{event.name}</h2>
+          </div>
+          <div>
+            <p className='time'>{event.time}</p>
+            <p className='date'>{event.date}</p>
+          </div>
+          <div>
+            <p className='venue'>Citi Field</p>
+          </div>
+          <div>
+            <button>Check In</button>
+          </div>
         </Link>
       </div>
       ))}
     </div>
+    </>
   );
 }
