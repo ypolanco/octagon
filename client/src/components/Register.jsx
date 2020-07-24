1c225c87be2c3d478323deee188baadcd94af3f7
JP Meyers <johnpmeyers20@yahoo.com>
2020-07-24 07:00:01 -0400

    redid css for register

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -19,7 +19,7 @@ export default class Register extends Component {
     const { username, email, password } = this.state;
     const { handleRegisterSubmit, history } = this.props;
     return (
-      <>
+      <div className='register-container'>
         <div className="register">
           <form
             className="form"
@@ -32,12 +32,12 @@ export default class Register extends Component {
                 email: "",
                 password: "",
               });
-
-            }}
-          >
-            <h3 className="header">Register</h3>
+            }}>
+            <div className='subform'>
+              {/* <h3 className="header">Register</h3> */}
+            <h1 className='header'>STADIO</h1>
             <label htmlFor="username" className="label">
-              Username
+              {/* Username */}
             </label>
 
             <input
@@ -45,14 +45,14 @@ export default class Register extends Component {
               id="username"
               type="text"
               name="username"
-
+              placeholder='Username'
               value={username}
               onChange={this.handleChange}
             />
 
             <br />
             <label htmlFor="email" className="label">
-              Email
+              {/* Email */}
             </label>
 
             <input
@@ -60,18 +60,20 @@ export default class Register extends Component {
               id="email"
               type="text"
               name="email"
+              placeholder='Email'
               value={email}
               onChange={this.handleChange}
             />
             <br />
             <label htmlFor="password" className="label-register">
-              Password
+              {/* Password */}
             </label>
             <input
               className="register-input"
               id="password"
               type="password"
               name="password"
+              placeholder='Password'
               value={password}
               onChange={this.handleChange}
             />
@@ -80,7 +82,7 @@ export default class Register extends Component {
             </div>
           </form>
         </div>
-      </>
+      </div>
     );
   }
 }
