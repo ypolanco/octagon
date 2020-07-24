cc2842156c70466e94e0269dca7e64513c88aceb
JP Meyers <johnpmeyers20@yahoo.com>
2020-07-24 11:18:27 -0400

    added single event styling

--- client/src/components/Event.jsx
+++ client/src/components/Event.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Banner from "./banner/Banner";
 import { getOneEvent } from "../services/events";
 import { Link } from "react-router-dom";
+import Footer from "./Footer";
 
 export default class Event extends Component {
   state = {
@@ -27,23 +28,33 @@ export default class Event extends Component {
     return (
       <div>
         <Banner />
-        <div>
-          <p>{event.name}</p>
-          <p>{event.date}</p>
-          <p>{event.time}</p>
-          <p>Citi Field</p>
-          <p>{event.section}</p>
-          <p>{event.row}</p>
-          <p>{event.seat}</p>
-          <p>
-            Witness the playoff showdown between the New York Giants and the New
-            England Patriots. It’s one of the most highly anticipated games of
-            the year!
-          </p>
-          <Link to={`/steps`}>
-            <button className="button">Check In</button>
-          </Link>
-        </div>
+        <div className='single-event-container'>
+          <div className='single-event'>
+            <div className='info-board-1'>
+              <Link to='/home'><img className='back-arrow' src='https://i.imgur.com/x60pPmo.png' alt='back arrow'/></Link>
+              <h2>{event.name}</h2>
+              <p>{event.date}</p>
+              <p>{event.time}</p>
+              <p>Citi Field</p>
+            </div>
+            <div className='info-board-2'>
+              <p>Section {event.section}</p>
+              <p>Row {event.row}</p>
+              <p>Seat {event.seat}</p>
+            </div>
+          </div>
+          <div>
+            <p>
+              Witness the playoff showdown between the New York Giants and the New
+              England Patriots. It’s one of the most highly anticipated games of
+              the year!
+            </p>
+            <Link to={`/steps`}>
+              <button className="button">Check In</button>
+            </Link>
+            <Footer />
+          </div>
+        </div>  
       </div>
     );
   }
