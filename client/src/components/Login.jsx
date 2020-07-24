e500dd590c8a1881af7b400355fada1b4dd15391
JP Meyers <johnpmeyers20@yahoo.com>
2020-07-23 20:13:29 -0400

    styled sign in page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -22,50 +22,60 @@ export default class Login extends Component {
     const { username, password } = this.state;
     const { handleLoginSubmit, history } = this.props;
     return (
-      <div className="login">
-        <form
-          className="form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleLoginSubmit(this.state);
-            history.push("/home");
-            this.setState({
-              username: "",
-              password: "",
-            });
-          }}
-        >
-          <h3 className="header">STADIO</h3>
-          <label htmlFor="username" className="label">
-            Username
-          </label>
-          <input
-            id="username"
-            type="text"
-            name="username"
-            value={username}
-            onChange={this.handleChange}
-            className="input"
-          />
-
-          <br />
-          <label htmlFor="password" className="label">
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-            className="input"
-          />
-          <br />
-          <button className="button">Login</button>
-          <Link to="/register">
-            <button className="button">Create Account</button>
-          </Link>
-        </form>
+      <div className="login-container">
+        <div className='login'>
+          <form
+            className="form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleLoginSubmit(this.state);
+              history.push("/home");
+              this.setState({
+                username: "",
+                password: "",
+              });
+            }}>
+            <div className='motha'>
+              <h1 className="header">STADIO</h1>
+              {/* <label htmlFor="username" className="label">
+                Username
+              </label> */}
+              <input
+                id="username"
+                type="text"
+                name="username"
+                placeholder='Email'
+                value={username}
+                onChange={this.handleChange}
+                className="input"
+              />
+              <br />
+              {/* <label htmlFor="password" className="label">
+                Password
+              </label> */}
+              <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder='Password'
+                value={password}
+                onChange={this.handleChange}
+                className="input"
+              />
+              <br />
+              <p>or log in with:</p>
+              <div className='social'>
+                <img src='https://i.imgur.com/22v0su3.png' alt='twitter' />
+                <img src='https://i.imgur.com/T46Okmj.png' alt='google' />
+                <img src='https://i.imgur.com/dYRamty.png' alt='facebook' />            
+              </div>
+              <br/>
+              <button className="button">Log In</button>
+              <p>don't have an account?<Link to="/register">  Sign Up</Link></p>
+              
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
